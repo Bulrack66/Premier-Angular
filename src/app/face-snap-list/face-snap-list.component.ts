4ff64a53186966791908f9e21b3b4c5c3f8df63b
Bulrack66 <faguowinien@gmail.com>
2022-12-11 13:24:56 +0100

    Ajout-modules

---
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrls: ['./face-snap-list.component.scss']
+})
+
+export class FaceSnapListComponent {
+
+  faceSnaps!: FaceSnap[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.faceSnaps = [
+      {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 20,
+      location: 'Malaga'
+      },
+      {
+
+      title:'Luke Skywalker',
+      description:'Un mec trés fort',
+      imageUrl:'https://cdn-media2.planete-starwars.com/news/60545-luke-skywalker-169-lg.jpg?w=768&f=w',
+      createdDate:new Date(),
+      snaps:0,
+      location:'Pekin'
+      },
+      {
+      title:'C-3PO',
+      description:'Un robot pas comme les autres',
+      imageUrl:'https://cdn-media.planete-starwars.com/news/63420-1-169-lg.jpg',
+      createdDate:new Date(),
+      snaps: 35,
+      location:'Washington'
+      }
+    ]
+  }
+}
