2d6ab5225535604600af30e5494315640c49353e
Bulrack66 <faguowinien@gmail.com>
2024-12-04 20:49:14 +0100

    T1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { filter, map } from "rxjs/operators";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +7,8 @@ import { filter, map } from "rxjs/operators";
 })
 export class AppComponent implements OnInit {
 
-  interval$!: Observable<string>;
 
   ngOnInit() {
-    this.interval$ = interval(1000).pipe(
-      filter(value => value % 3 === 0),
-      map(value => value % 2 === 0 ?
-        `Je suis ${value} et je suis pair` :
-        `Je suis ${value} et je suis impair`
-      ),
-    );
+
   }
 }
